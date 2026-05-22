1ff590977f7f4d562e09f4ff7b8e37213b0611fb
GabrielaLopesFerreira <24214309@aluno.univesp.br>
2026-05-22 20:50:00 -0300

    V2.1

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-from flask import Flask #importa a classe Flask do módulo flask para criar a aplicação web.
-from route import routes   # 👈 importa o Blueprint corretamente
-
+from flask import Flask
+from route import routes
+import os
 
 app = Flask(__name__)
 app.register_blueprint(routes)
 
-
-if __name__ == '__main__':
-    app.run(debug=True) #verifica se o script está sendo executado diretamente e, em caso afirmativo, inicia o servidor de desenvolvimento do Flask com o modo de depuração ativado.
-
-app.run()
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
